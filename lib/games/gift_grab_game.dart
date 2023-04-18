85a9921b01e8217bdf80fa8ce595d673c79b16d3
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-04-18 21:41:54 +0500

    Crested input directory
    Created:
    joystick.dart

--- lib/games/gift_grab_game.dart
+++ lib/games/gift_grab_game.dart
@@ -1,13 +1,17 @@
 import 'package:flame/game.dart';
 import 'package:gift_grab_game_with_flame/components/background_component.dart';
 import 'package:gift_grab_game_with_flame/components/santa_component.dart';
+import 'package:gift_grab_game_with_flame/inputs/joystick.dart';
 
-class GiftGrabGame extends FlameGame {
+class GiftGrabGame extends FlameGame with HasDraggables {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
     add(BackgroundComponent());
-    add(SantaComponent());
+
+    add(SantaComponent(joystick: joystick));
+
+    add(joystick);
   }
 }
